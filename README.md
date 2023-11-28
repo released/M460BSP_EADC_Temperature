3501ac9a95e9634151f2781468ad3cf2cc8100f9
Released <released.chen@gmail.com>
2023-11-28 14:31:17 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# M460BSP_EADC_Temperature
+ M460BSP_EADC_Temperature
---
+++ SampleCode/Template/main.c
@@ -0,0 +1,662 @@
+/*_____ I N C L U D E S ____________________________________________________*/
+#include <stdio.h>
+#include <string.h>
+#include "NuMicro.h"
+
+#include "misc_config.h"
+
+/*_____ D E C L A R A T I O N S ____________________________________________*/
+
+struct flag_32bit flag_PROJ_CTL;
+#define FLAG_PROJ_TIMER_PERIOD_1000MS                 	(flag_PROJ_CTL.bit0)
+#define FLAG_PROJ_ADC_CONVERT                  			(flag_PROJ_CTL.bit1)
+#define FLAG_PROJ_REVERSE2                 				(flag_PROJ_CTL.bit2)
+#define FLAG_PROJ_REVERSE3                              (flag_PROJ_CTL.bit3)
+#define FLAG_PROJ_REVERSE4                              (flag_PROJ_CTL.bit4)
+#define FLAG_PROJ_REVERSE5                              (flag_PROJ_CTL.bit5)
+#define FLAG_PROJ_REVERSE6                              (flag_PROJ_CTL.bit6)
+#define FLAG_PROJ_REVERSE7                              (flag_PROJ_CTL.bit7)
+
+
+/*_____ D E F I N I T I O N S ______________________________________________*/
+
+volatile unsigned int counter_systick = 0;
+volatile uint32_t counter_tick = 0;
+
+
+volatile uint32_t g_u32AdcIntFlag;
+
+volatile uint32_t g_u32Vref;
+
+/*_____ M A C R O S ________________________________________________________*/
+
+/*_____ F U N C T I O N S __________________________________________________*/
+
+unsigned int get_systick(void)
+{
+	return (counter_systick);
+}
+
+void set_systick(unsigned int t)
+{
+	counter_systick = t;
+}
+
+void systick_counter(void)
+{
+	counter_systick++;
+}
+
+void SysTick_Handler(void)
+{
+
+    systick_counter();
+
+    if (get_systick() >= 0xFFFFFFFF)
+    {
+        set_systick(0);      
+    }
+
+    // if ((get_systick() % 1000) == 0)
+    // {
+       
+    // }
+
+    #if defined (ENABLE_TICK_EVENT)
+    TickCheckTickEvent();
+    #endif    
+}
+
+void SysTick_delay(unsigned int delay)
+{  
+    
+    unsigned int tickstart = get_systick(); 
+    unsigned int wait = delay; 
+
+    while((get_systick() - tickstart) < wait) 
+    { 
+    } 
+
+}
+
+void SysTick_enable(unsigned int ticks_per_second)
+{
+    set_systick(0);
+    if (SysTick_Config(SystemCoreClock / ticks_per_second))
+    {
+        /* Setup SysTick Timer for 1 second interrupts  */
+        printf("Set system tick error!!\n");
+        while (1);
+    }
+
+    #if defined (ENABLE_TICK_EVENT)
+    TickInitTickEvent();
+    #endif
+}
+
+uint32_t get_tick(void)
+{
+	return (counter_tick);
+}
+
+void set_tick(uint32_t t)
+{
+	counter_tick = t;
+}
+
+void tick_counter(void)
+{
+	counter_tick++;
+    if (get_tick() >= 60000)
+    {
+        set_tick(0);
+    }
+}
+
+void delay_ms(uint16_t ms)
+{
+	#if 1
+    uint32_t tickstart = get_tick();
+    uint32_t wait = ms;
+	uint32_t tmp = 0;
+	
+    while (1)
+    {
+		if (get_tick() > tickstart)	// tickstart = 59000 , tick_counter = 60000
+		{
+			tmp = get_tick() - tickstart;
+		}
+		else // tickstart = 59000 , tick_counter = 2048
+		{
+			tmp = 60000 -  tickstart + get_tick();
+		}		
+		
+		if (tmp > wait)
+			break;
+    }
+	
+	#else
+	TIMER_Delay(TIMER0, 1000*ms);
+	#endif
+}
+
+void EADC00_IRQHandler(void)
+{
+    g_u32AdcIntFlag = 1;
+    /* Clear the A/D ADINT0 interrupt flag */
+    EADC_CLR_INT_FLAG(EADC0, EADC_STATUS2_ADIF0_Msk);
+}
+
+/*
+    Bandgap/ VREF = ADC Data/ 4095
+    Vbg = 1.210 (V) 
+*/
+
+void EADC_BandGap_Convert(void)
+{
+
+    int32_t  i32ConversionData;
+    uint32_t u32TimeOutCnt;
+
+ 
+    /* Clear the A/D ADINT0 interrupt flag for safe */
+    EADC_CLR_INT_FLAG(EADC0, EADC_STATUS2_ADIF0_Msk);
+
+    /* Enable the sample module 16 interrupt.  */
+    EADC_ENABLE_INT(EADC0, BIT0);//Enable sample module A/D ADINT0 interrupt.
+    EADC_ENABLE_SAMPLE_MODULE_INT(EADC0, 0, BIT16);//Enable sample module 16 interrupt.
+    NVIC_EnableIRQ(EADC00_IRQn);
+
+    /* Reset the ADC interrupt indicator and trigger sample module 16 to start A/D conversion */
+    g_u32AdcIntFlag = 0;
+    EADC_START_CONV(EADC0, BIT16);
+
+    /* Wait EADC conversion done */
+    u32TimeOutCnt = SystemCoreClock; /* 1 second time-out */
+    while(g_u32AdcIntFlag == 0)
+    {
+        if(--u32TimeOutCnt == 0)
+        {
+            printf("Wait for EADC conversion done time-out!\n");
+            return;
+        }
+    }
+
+    /* Disable the ADINT0 interrupt */
+    EADC_DISABLE_INT(EADC0, BIT0);
+
+    /* Get the conversion result of the sample module 16 */
+    i32ConversionData = EADC_GET_CONV_DATA(EADC0, 16);
+
+    g_u32Vref = (1.210 * 4095 * 1000) / i32ConversionData ;
+
+    printf("Band-gap: 0x%4X(%4d),VREF : %4dmv\r\n", i32ConversionData, i32ConversionData ,g_u32Vref);
+
+}
+
+/*
+    Temperature Coefficient  = -1.82
+    Temperature sensor offset voltage = 674 mv
+
+    Vtemp (mV) = Tc *　Temp + VTEMP_OS  (mV) 
+    => Temp = (Vtemp (mV) - VTEMP_OS  (mV) ) / Tc
+*/
+void EADC_Temperature_Convert(void)
+{
+
+    int32_t  i32ConversionData;
+    uint32_t u32TimeOutCnt;
+    float fTemperature;
+
+   /* Clear the A/D ADINT0 interrupt flag for safe */
+    EADC_CLR_INT_FLAG(EADC0, EADC_STATUS2_ADIF0_Msk);
+
+    /* Enable the sample module 17 interrupt.  */
+    EADC_ENABLE_INT(EADC0, BIT0);//Enable sample module A/D ADINT0 interrupt.
+    EADC_ENABLE_SAMPLE_MODULE_INT(EADC0, 0, BIT17);//Enable sample module 17 interrupt.
+    NVIC_EnableIRQ(EADC00_IRQn);
+
+    /* Reset the ADC interrupt indicator and trigger sample module 17 to start A/D conversion */
+    g_u32AdcIntFlag = 0;
+    EADC_START_CONV(EADC0, BIT17);
+
+    /* Wait EADC conversion done */
+    u32TimeOutCnt = SystemCoreClock; /* 1 second time-out */
+    while(g_u32AdcIntFlag == 0)
+    {
+        if(--u32TimeOutCnt == 0)
+        {
+            printf("Wait for EADC conversion done time-out!\n");
+            return;
+        }
+    }
+
+    /* Disable the ADINT0 interrupt */
+    EADC_DISABLE_INT(EADC0, BIT0);
+
+    /* Get the conversion result of the sample module 17 */
+    i32ConversionData = EADC_GET_CONV_DATA(EADC0, 17);
+
+    /* The equation of converting to real temperature is as below
+     * (25+(((float)i32ConversionData/4095*3300)-674)/(-1.82)), 3300 means ADCVREF=3.3V
+     * If ADCREF set to 1.6V, the equation should be updated as below
+     * (25+(((float)i32ConversionData/4095*1600)-674)/(-1.82)), 1600 means ADCVREF=1.6V
+     */
+    fTemperature = (25+(((float)i32ConversionData/4095*g_u32Vref)-674)/(-1.82)) ;
+    
+    printf("temperature sensor: 0x%4X(%4d),VTemp:%2.4f\r\n", i32ConversionData, i32ConversionData, fTemperature);
+}
+
+void EADC_Init(void)
+{
+
+    int32_t  i32Err;
+
+    /* Set input mode as single-end and enable the A/D converter */
+    i32Err = EADC_Open(EADC0, EADC_CTL_DIFFEN_SINGLE_END);
+
+    /* Check EADC global error code. */
+    if (i32Err != 0)
+    {
+        if (i32Err == EADC_CAL_ERR)
+        {
+            printf("EADC has calibration error.\n");
+            return;
+        }
+        else if (i32Err == EADC_CLKDIV_ERR)
+        {
+            printf("EADC clock frequency is configured error.\n");
+            return;
+        }
+        else
+        {
+            printf("EADC has operation error.\n");
+            return;
+        }
+    }
+
+    /* Set sample module 16 external sampling time to 0xFF */
+    EADC_SetExtendSampleTime(EADC0, 16, 0xFF);
+
+    /* Set sample module 17 external sampling time to 0xFF */
+    EADC_SetExtendSampleTime(EADC0, 17, 0xFF);
+
+}
+
+
+//
+// check_reset_source
+//
+uint8_t check_reset_source(void)
+{
+    uint32_t src = SYS_GetResetSrc();
+
+    /*
+        0x02 = M467.  
+        0x03 = M463. 
+    */
+
+    if ((SYS->CSERVER & SYS_CSERVER_VERSION_Msk) == 0x2)  
+    {
+		printf("PN : M467\r\n");
+    }
+    else if  ((SYS->CSERVER & SYS_CSERVER_VERSION_Msk) == 0x3)
+    {
+		printf("PN : M463\r\n");
+    }
+
+    SYS->RSTSTS |= 0x1FF;
+    printf("Reset Source <0x%08X>\r\n", src);
+
+    #if 1   //DEBUG , list reset source
+    if (src & BIT0)
+    {
+        printf("0)POR Reset Flag\r\n");       
+    }
+    if (src & BIT1)
+    {
+        printf("1)NRESET Pin Reset Flag\r\n");       
+    }
+    if (src & BIT2)
+    {
+        printf("2)WDT Reset Flag\r\n");       
+    }
+    if (src & BIT3)
+    {
+        printf("3)LVR Reset Flag\r\n");       
+    }
+    if (src & BIT4)
+    {
+        printf("4)BOD Reset Flag\r\n");       
+    }
+    if (src & BIT5)
+    {
+        printf("5)System Reset Flag \r\n");       
+    }
+    if (src & BIT6)
+    {
+        printf("6)HRESET Reset Flag \r\n");       
+    }
+    if (src & BIT7)
+    {
+        printf("7)CPU Reset Flag\r\n");       
+    }
+    if (src & BIT8)
+    {
+        printf("8)CPU Lockup Reset Flag\r\n");       
+    }
+    #endif
+    
+    if (src & SYS_RSTSTS_PORF_Msk) {
+        SYS_ClearResetSrc(SYS_RSTSTS_PORF_Msk);
+        
+        printf("power on from POR\r\n");
+        return FALSE;
+    }    
+    else if (src & SYS_RSTSTS_PINRF_Msk)
+    {
+        SYS_ClearResetSrc(SYS_RSTSTS_PINRF_Msk);
+        
+        printf("power on from nRESET pin\r\n");
+        return FALSE;
+    } 
+    else if (src & SYS_RSTSTS_WDTRF_Msk)
+    {
+        SYS_ClearResetSrc(SYS_RSTSTS_WDTRF_Msk);
+        
+        printf("power on from WDT Reset\r\n");
+        return FALSE;
+    }    
+    else if (src & SYS_RSTSTS_LVRF_Msk)
+    {
+        SYS_ClearResetSrc(SYS_RSTSTS_LVRF_Msk);
+        
+        printf("power on from LVR Reset\r\n");
+        return FALSE;
+    }    
+    else if (src & SYS_RSTSTS_BODRF_Msk)
+    {
+        SYS_ClearResetSrc(SYS_RSTSTS_BODRF_Msk);
+        
+        printf("power on from BOD Reset\r\n");
+        return FALSE;
+    }    
+    else if (src & SYS_RSTSTS_MCURF_Msk)
+    {
+        SYS_ClearResetSrc(SYS_RSTSTS_MCURF_Msk);
+        
+        printf("power on from System Reset\r\n");
+        return FALSE;
+    } 
+    else if (src & SYS_RSTSTS_CPURF_Msk)
+    {
+        SYS_ClearResetSrc(SYS_RSTSTS_CPURF_Msk);
+
+        printf("power on from CPU reset\r\n");
+        return FALSE;         
+    }    
+    else if (src & SYS_RSTSTS_CPULKRF_Msk)
+    {
+        SYS_ClearResetSrc(SYS_RSTSTS_CPULKRF_Msk);
+        
+        printf("power on from CPU Lockup Reset\r\n");
+        return FALSE;
+    }   
+    
+    printf("power on from unhandle reset source\r\n");
+    return FALSE;
+}
+
+void TMR1_IRQHandler(void)
+{
+	
+    if(TIMER_GetIntFlag(TIMER1) == 1)
+    {
+        TIMER_ClearIntFlag(TIMER1);
+		tick_counter();
+
+		if ((get_tick() % 1000) == 0)
+		{
+            FLAG_PROJ_TIMER_PERIOD_1000MS = 1;//set_flag(flag_timer_period_1000ms ,ENABLE);
+		}
+
+		if ((get_tick() % 50) == 0)
+		{
+
+		}	
+    }
+}
+
+void TIMER1_Init(void)
+{
+    TIMER_Open(TIMER1, TIMER_PERIODIC_MODE, 1000);
+    TIMER_EnableInt(TIMER1);
+    NVIC_EnableIRQ(TMR1_IRQn);	
+    TIMER_Start(TIMER1);
+}
+
+void loop(void)
+{
+	static uint32_t LOG1 = 0;
+	// static uint32_t LOG2 = 0;
+
+    if ((get_systick() % 1000) == 0)
+    {
+        // printf("%s(systick) : %4d\r\n",__FUNCTION__,LOG2++);    
+    }
+
+    if (FLAG_PROJ_TIMER_PERIOD_1000MS)//(is_flag_set(flag_timer_period_1000ms))
+    {
+        FLAG_PROJ_TIMER_PERIOD_1000MS = 0;//set_flag(flag_timer_period_1000ms ,DISABLE);
+
+        // printf("%s(timer) : %4d\r\n",__FUNCTION__,LOG1++);
+        FLAG_PROJ_ADC_CONVERT = 1;
+        PH4 ^= 1;             
+    }
+
+    if (FLAG_PROJ_ADC_CONVERT)
+    {
+        FLAG_PROJ_ADC_CONVERT = 0;
+        EADC_BandGap_Convert();
+        EADC_Temperature_Convert();
+    }
+
+}
+
+void UARTx_Process(void)
+{
+	uint8_t res = 0;
+	res = UART_READ(UART0);
+
+	if (res > 0x7F)
+	{
+		printf("invalid command\r\n");
+	}
+	else
+	{
+		printf("press : %c\r\n" , res);
+		switch(res)
+		{
+			case '1':
+				break;
+
+			case 'X':
+			case 'x':
+			case 'Z':
+			case 'z':
+                SYS_UnlockReg();
+				// NVIC_SystemReset();	// Reset I/O and peripherals , only check BS(FMC_ISPCTL[1])
+                // SYS_ResetCPU();     // Not reset I/O and peripherals
+                SYS_ResetChip();    // Reset I/O and peripherals ,  BS(FMC_ISPCTL[1]) reload from CONFIG setting (CBS)	
+				break;
+		}
+	}
+}
+
+void UART0_IRQHandler(void)
+{
+    if(UART_GET_INT_FLAG(UART0, UART_INTSTS_RDAINT_Msk | UART_INTSTS_RXTOINT_Msk))     /* UART receive data available flag */
+    {
+        while(UART_GET_RX_EMPTY(UART0) == 0)
+        {
+			UARTx_Process();
+        }
+    }
+
+    if(UART0->FIFOSTS & (UART_FIFOSTS_BIF_Msk | UART_FIFOSTS_FEF_Msk | UART_FIFOSTS_PEF_Msk | UART_FIFOSTS_RXOVIF_Msk))
+    {
+        UART_ClearIntFlag(UART0, (UART_INTSTS_RLSINT_Msk| UART_INTSTS_BUFERRINT_Msk));
+    }	
+}
+
+void UART0_Init(void)
+{
+    SYS_ResetModule(UART0_RST);
+
+    /* Configure UART0 and set UART0 baud rate */
+    UART_Open(UART0, 115200);
+
+	/* Set UART receive time-out */
+	UART_SetTimeoutCnt(UART0, 20);
+
+	UART0->FIFO &= ~UART_FIFO_RFITL_4BYTES;
+	UART0->FIFO |= UART_FIFO_RFITL_8BYTES;
+
+	/* Enable UART Interrupt - */
+	UART_ENABLE_INT(UART0, UART_INTEN_RDAIEN_Msk | UART_INTEN_TOCNTEN_Msk | UART_INTEN_RXTOIEN_Msk);
+	
+	NVIC_EnableIRQ(UART0_IRQn);
+
+	#if (_debug_log_UART_ == 1)	//debug
+	printf("\r\nCLK_GetCPUFreq : %8d\r\n",CLK_GetCPUFreq());
+	printf("CLK_GetHCLKFreq : %8d\r\n",CLK_GetHCLKFreq());
+	printf("CLK_GetHXTFreq : %8d\r\n",CLK_GetHXTFreq());
+	printf("CLK_GetLXTFreq : %8d\r\n",CLK_GetLXTFreq());	
+	printf("CLK_GetPCLK0Freq : %8d\r\n",CLK_GetPCLK0Freq());
+	printf("CLK_GetPCLK1Freq : %8d\r\n",CLK_GetPCLK1Freq());
+	printf("CLK_GetHCLKFreq : %8d\r\n",CLK_GetHCLKFreq());    	
+
+//    printf("Product ID 0x%8X\n", SYS->PDID);
+	
+	#endif	
+
+    #if 0
+    printf("FLAG_PROJ_TIMER_PERIOD_1000MS : 0x%2X\r\n",FLAG_PROJ_TIMER_PERIOD_1000MS);
+    printf("FLAG_PROJ_REVERSE1 : 0x%2X\r\n",FLAG_PROJ_REVERSE1);
+    printf("FLAG_PROJ_REVERSE2 : 0x%2X\r\n",FLAG_PROJ_REVERSE2);
+    printf("FLAG_PROJ_REVERSE3 : 0x%2X\r\n",FLAG_PROJ_REVERSE3);
+    printf("FLAG_PROJ_REVERSE4 : 0x%2X\r\n",FLAG_PROJ_REVERSE4);
+    printf("FLAG_PROJ_REVERSE5 : 0x%2X\r\n",FLAG_PROJ_REVERSE5);
+    printf("FLAG_PROJ_REVERSE6 : 0x%2X\r\n",FLAG_PROJ_REVERSE6);
+    printf("FLAG_PROJ_REVERSE7 : 0x%2X\r\n",FLAG_PROJ_REVERSE7);
+    #endif
+
+}
+
+void GPIO_Init (void)
+{
+    SET_GPIO_PH4();
+    SET_GPIO_PH5();
+    SET_GPIO_PH6();
+
+	//EVM LED
+	GPIO_SetMode(PH,BIT4,GPIO_MODE_OUTPUT);
+	GPIO_SetMode(PH,BIT5,GPIO_MODE_OUTPUT);
+	GPIO_SetMode(PH,BIT6,GPIO_MODE_OUTPUT);
+	
+}
+
+void SYS_Init(void)
+{
+    /* Unlock protected registers */
+    SYS_UnlockReg();
+
+    /* Set XT1_OUT(PF.2) and XT1_IN(PF.3) to input mode */
+//    PF->MODE &= ~(GPIO_MODE_MODE2_Msk | GPIO_MODE_MODE3_Msk);
+    
+    CLK_EnableXtalRC(CLK_PWRCTL_HIRCEN_Msk);
+    CLK_WaitClockReady(CLK_STATUS_HIRCSTB_Msk);
+
+//    CLK_EnableXtalRC(CLK_PWRCTL_HXTEN_Msk);
+//    CLK_WaitClockReady(CLK_STATUS_HXTSTB_Msk);
+
+//    CLK_EnableXtalRC(CLK_PWRCTL_LIRCEN_Msk);
+//    CLK_WaitClockReady(CLK_STATUS_LIRCSTB_Msk);
+
+//    CLK_EnableXtalRC(CLK_PWRCTL_LXTEN_Msk);
+//    CLK_WaitClockReady(CLK_STATUS_LXTSTB_Msk);
+
+    /* Set PCLK0/PCLK1 to HCLK/2 */
+    CLK->PCLKDIV = (CLK_PCLKDIV_APB0DIV_DIV2 | CLK_PCLKDIV_APB1DIV_DIV2);
+
+    /* Enable all GPIO clock */
+    CLK->AHBCLK0 |= CLK_AHBCLK0_GPACKEN_Msk | CLK_AHBCLK0_GPBCKEN_Msk | CLK_AHBCLK0_GPCCKEN_Msk | CLK_AHBCLK0_GPDCKEN_Msk |
+                    CLK_AHBCLK0_GPECKEN_Msk | CLK_AHBCLK0_GPFCKEN_Msk | CLK_AHBCLK0_GPGCKEN_Msk | CLK_AHBCLK0_GPHCKEN_Msk;
+    CLK->AHBCLK1 |= CLK_AHBCLK1_GPICKEN_Msk | CLK_AHBCLK1_GPJCKEN_Msk;
+
+
+    /* Set core clock to 200MHz */
+    CLK_SetCoreClock(200000000);
+
+    /* Enable UART clock */
+    CLK_EnableModuleClock(UART0_MODULE);
+    /* Select UART clock source from HXT */
+    CLK_SetModuleClock(UART0_MODULE, CLK_CLKSEL1_UART0SEL_HIRC, CLK_CLKDIV0_UART0(1));
+
+    /* Set GPB multi-function pins for UART0 RXD and TXD */
+    SET_UART0_RXD_PB12();
+    SET_UART0_TXD_PB13();
+
+    CLK_EnableModuleClock(TMR1_MODULE);
+    CLK_SetModuleClock(TMR1_MODULE, CLK_CLKSEL1_TMR1SEL_HIRC, 0);
+	
+    /* Enable EADC0 module clock */
+    CLK_EnableModuleClock(EADC0_MODULE);
+
+    /* Set EADC0 clock divider as 12 */
+    CLK_SetModuleClock(EADC0_MODULE, CLK_CLKSEL0_EADC0SEL_PLL_DIV2, CLK_CLKDIV0_EADC0(12));
+    /* Enable temperature sensor */
+    SYS->IVSCTL |= SYS_IVSCTL_VTEMPEN_Msk;
+
+    /* Update System Core Clock */
+    /* User can use SystemCoreClockUpdate() to calculate SystemCoreClock. */
+    SystemCoreClockUpdate();
+
+    /* Lock protected registers */
+    SYS_LockReg();
+}
+
+/*
+ * This is a template project for M480 series MCU. Users could based on this project to create their
+ * own application without worry about the IAR/Keil project settings.
+ *
+ * This template application uses external crystal as HCLK source and configures UART0 to print out
+ * "Hello World", users may need to do extra system configuration based on their system design.
+ */
+
+int main()
+{
+    SYS_Init();
+
+	GPIO_Init();
+	UART0_Init();
+	TIMER1_Init();
+    check_reset_source();
+
+    SysTick_enable(1000);
+    #if defined (ENABLE_TICK_EVENT)
+    TickSetTickEvent(1000, TickCallback_processA);  // 1000 ms
+    TickSetTickEvent(5000, TickCallback_processB);  // 5000 ms
+    #endif
+
+    EADC_Init();
+
+    /* Got no where to go, just loop forever */
+    while(1)
+    {
+        loop();
+
+    }
+}
+
+/*** (C) COPYRIGHT 2016 Nuvoton Technology Corp. ***/
